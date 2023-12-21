2b8e46c1e4c0604c21ab7f4149b436260749b366
Eunji <eeunjiilee@gmail.com>
2023-12-21 13:02:41 +0900

    feat: gradient container component

---
+++ styles/ui.style.ts
@@ -0,0 +1,17 @@
+// test
+
+import styled from '@emotion/styled';
+
+export const GradientContainer = styled.div`
+  position: relative;
+
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 40px;
+    background: linear-gradient(180deg, transparent 0, #fff 60%);
+  }
+`;
