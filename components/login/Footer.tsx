a93347e2817a42b6b1e6eebbc6461956885c478e
Eunji <eeunjiilee@gmail.com>
2024-01-24 12:25:19 +0900

    design: change footer text

--- components/login/Footer.tsx
+++ components/login/Footer.tsx
@@ -1,10 +1,11 @@
 /** @jsxImportSource @emotion/react */
 import Link from 'next/link';
-import { container, emp, text } from './Footer.style';
+import { container, emp, base, text } from './Footer.style';
 
 function LoginFooter() {
   return (
     <div css={container}>
+      <span css={[text, base]}>아직 회원이 아니라면?</span>
       <Link href={'/join'} css={[text, emp]}>
         회원가입하기
       </Link>
